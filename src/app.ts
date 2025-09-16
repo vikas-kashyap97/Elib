1b6f0903d317b28b758df1839d8e9b76b6a3d387
vikas-kashyap97 <your_github_email@example.com>
2025-09-16 12:05:23 +0530

    change create user status code to 201

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/users", userRouter);
+
 // Global error handler
 
 app.use(globalErrorHandler);
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -49,11 +49,11 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     });
 
     // response
-    res.json({ accessToken: token });
+    res.status(201).json({ accessToken: token });
   } catch (err) {
     console.error("Error while generating JWT Tokens:", err);
     return next(createHttpError(500, "Error while generating JWT Tokens."));
   }
 };
 
-export { createUser };
+export { createUser, loginUser };
