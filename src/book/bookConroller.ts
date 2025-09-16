46ae001efdaa2bd4f107163ae3be95550720ef18
vikas-kashyap97 <your_github_email@example.com>
2025-09-16 12:48:45 +0530

    setup multer

--- src/book/bookConroller.ts
+++ src/book/bookConroller.ts
@@ -1,7 +1,30 @@
-import { NextFunction, Response, Request } from "express";
+import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
+import BookModel from "./bookModel";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
-  res.json({});
+  const { title, author, genre, coverImage, file } = req.body;
+
+  // validation
+  if (!title || !author || !genre || !coverImage || !file) {
+    const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
+
+  try {
+    const newBook = await BookModel.create({
+      title,
+      author,
+      genre,
+      coverImage,
+      file,
+    });
+
+    res.status(201).json(newBook);
+  } catch (err) {
+    console.error("Error while creating book:", err);
+    return next(createHttpError(500, "Error while creating book."));
+  }
 };
 
 export default createBook;
--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,8 +1,22 @@
 import express from "express";
 import createBook from "./bookConroller";
+import multer from "multer";
+import path from "node:path";
 const bookRouter = express.Router();
 
+const upload = multer({
+  dest: path.resolve(__dirname, "../../public/data/uploads"),
+  limits: { fileSize: 3e7 }, // 30MB
+});
+
 // Routes
-bookRouter.post("/", createBook);
+bookRouter.post(
+  "/",
+  upload.fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "file", maxCount: 1 },
+  ]),
+  createBook
+);
 
 export default bookRouter;
